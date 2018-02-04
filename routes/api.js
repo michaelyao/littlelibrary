532774d98dfe554da4a6e1fa8eac4e8d78cc93c4
Michael Yao <myao@capricorn.local>
2018-02-04 10:38:51 -0800

    change db field

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,28 @@ var router = express.Router();
 router.post('/book', function(req, res, next) {
     console.log("add book router");
     console.log(JSON.stringify(req.body));
+    var bookInfo = {};
+    if( "title" in req.body_){
+        bookInfo['Book_name'] = req.body["title"];
+    }
+    if("imageUrl" in req.body){
+        bookInfo['image_url'] = req.body["imageUrl"];
+    }
+    if("ID" in req.body){
+        bookInfo['Scanned_code'] = req.body["ID"];
+    }
+
+    if("codeType" in req.body){
+        bookInfo['code_type'] = req.body["codeType"];
+    }
+    if("ASIN" in req.body){
+        bookInfo['ASIN'] = req.body["ASIN"];
+    }
+    if("pageUrl" in req.body){
+        bookInfo['book_url'] = req.body["pageUrl"];
+    }
+
+
     Book.addBook(req.body,function(err,count){
         if(err)
         {
